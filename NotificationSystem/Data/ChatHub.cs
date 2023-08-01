acb7156b0e6e67f458263eab214ac3287bcc921d
felixyangyicheng <admin-fr06ab@FRLT00053>
2023-08-01 09:15:07 +0200

    Ajoutez des fichiers projet.

---
+++ NotificationSystem/Data/ChatHub.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace NotificationSystem.Data
+{
+
+    public class ChatHub :Hub
+    {
+
+
+        public async Task SendMessage(string sender, string receiver, string msgTitle, string msgBody) 
+        {
+            await Clients.All.SendAsync("ReceiveMessage", sender, receiver, msgTitle, msgBody);
+        }
+    }
+}
---
+++ NotificationSystem/Data/NotificationMessage.cs
@@ -0,0 +1,14 @@
+﻿namespace NotificationSystem.Data
+{
+    public class NotificationMessage
+    {
+        public int MsgId { get; set; } = 0;
+        public string ReceiverName { get; set; } = "";
+        public string SenderName { get; set; } = "";
+        public string MsgTitle { get; set; } = "";
+        public string MsgBody { get; set; } = "";
+        public DateTime MsgDate { get; set; } = DateTime.Now;
+        public string  MsgDateSt => this.MsgDate.ToString("dd-MM-yyyy");
+
+    }
+}
